c616df6ce0cefef0233483731e76f92a64c9ecf5
JackPinheiro <jackfilipepinheiro@gmail.com>
2022-10-27 18:20:30 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '343e931b-ce0a-4c6d-bf17-9322eb6be8de';
+export const MODAL_CLOSE_SEC= 2.5;
